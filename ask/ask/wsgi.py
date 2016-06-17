b7c8277df8c5e32d2f49e0c86c700280e79d9c36
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-17 21:10:43 +0300

    Django fix 888

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys   
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
-
+sys.path.append('/home/box/web/ask/')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
-apt-get update
-apt-get install -y Nginx
-apt-get install -y gunicorn
-apt-get install -y python-pip
+sudo apt-get update
+sudo apt-get install -y Nginx
+sudo apt-get install -y gunicorn
+sudo apt-get install -y python-pip
 sudo pip install --upgrade django
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/wsgi.py /etc/gunicorn.d/wsgi.py
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask/ask wsgi &
+sudo gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask/ask/ wsgi &
