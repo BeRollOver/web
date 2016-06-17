d62a7c5a69a2c855c40e2c8e42b0b78ce1a103eb
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-17 09:34:29 +0300

    Django 1

---
+++ ask/ask/ask/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', views.test),
+    url(r'^singup/', views.test),
+    url(r'^question/', include('qa.urls')),
+    url(r'^ask/', views.test),
+    url(r'^popular/', views.test),
+    url(r'^new/', views.test),
+)
