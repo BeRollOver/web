3c82fdf570d39dfbe716b337b10bfb943dcad688
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-17 10:28:43 +0300

    Django fix 3В

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart﻿﻿
-sudo gunicorn -b 0.0.0.0:8000 hello:wsgi_application &
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask/ask wsgi &
