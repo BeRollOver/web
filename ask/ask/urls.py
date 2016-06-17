9028cf7a06f35aae357fe15d7f62659448f8f07b
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-17 22:17:52 +0300

    Django fix 9

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^/$', include(admin.site.urls)),
     url(r'^login/', 'qa.views.test'),
     url(r'^singup/', 'qa.views.test'),
     url(r'^question/', include('qa.urls')),
