23489af6bed718318163fc17faa67cc3fffd72f7
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-17 07:58:27 +0300

    wgsi 2

--- hello.py
+++ hello.py
@@ -5,8 +5,7 @@ def wsgi_application(environ, start_response):
         ('Content-Type', 'text/plain')
         ]
     
-    args = environ.QUERY_STRING.split("&")
-    body = '\n'.join(args)
-    
+    body = "\r\n".join(environ['QUERY_STRING'].split("&")) 
+
     start_response(status, headers )
     return [ body ]
