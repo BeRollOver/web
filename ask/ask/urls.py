704044613d93cf55915f9a915d533ffef7cba89d
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-17 09:48:29 +0300

    Django fix 23

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,4 +1,18 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 
-urlpatterns = [
-]
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', views.test),
+    url(r'^singup/', views.test),
+    url(r'^question/', include('qa.urls')),
+    url(r'^ask/', views.test),
+    url(r'^popular/', views.test),
+    url(r'^new/', views.test),
+)
