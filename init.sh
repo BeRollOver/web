512ae64388b2b66e3f0d36ad4832e4cb5c6e6d7b
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-17 09:41:01 +0300

    Django fix 2

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart﻿﻿
-sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_application &
+sudo gunicorn -b 0.0.0.0:8000 hello:wsgi_application &
