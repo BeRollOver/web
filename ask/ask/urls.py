b8605d69bfc28caec0434ff6369f730d977e69c4
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-17 21:29:15 +0300

    Django fix 4887

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,15 +3,18 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from . import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^login/', qa.views.test),
-    url(r'^singup/', qa.views.test),
-    url(r'^question/[0-9]+/$', qa.views.test),
-    url(r'^ask/', qa.views.test),
-    url(r'^popular/', qa.views.test),
-    url(r'^new/', qa.views.test),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', ask.views.test),
+    url(r'^singup/', ask.views.test),
+    url(r'^question/', include('qa.urls')),
+    url(r'^ask/', ask.views.test),
+    url(r'^popular/', ask.views.test),
+    url(r'^new/', ask.views.test),
 )
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'[0-9]+', views.test, name="test"),
+    url(r'[0-9]+/$', views.test, name="test"),
 ]
