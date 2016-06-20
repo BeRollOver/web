01849c1302e58bc2b65415b8c35b8c794c2751ed
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-20 03:41:16 +0300

    Models API using

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,22 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class QuestionManager(models.Manager):                                          
-    def new(self):                                                              
-        from django.db import connection
-        cursor = connection.cursor()
-        cursor.execute("""
-            SELECT *
-            FROM mybase
-            ORDER BY added_at DESC""")
-        return cursor.fetchone()
-    def popular():                                                          
-        from django.db import connection
-        cursor = connection.cursor()
-        cursor.execute("""
-            SELECT *
-            FROM mybase
-            ORDER BY rating DESC""")
-        return cursor.fetchone()
+    def new(self):
+        return self.order_by("-added_at")[:5]
+    def popular(self):
+        return self.order_by("-rating")[:5]
 
 class Question(models.Model):
     objects = QuestionManager()
