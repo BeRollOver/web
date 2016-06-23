a29038b4d05fbef26d049334302d250a1a58e956
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-23 19:44:09 +0300

    Django fix

--- ask/install.sh
+++ ask/install.sh
@@ -1,5 +1,10 @@
 sudo apt-get update
+sudo apt-get install -y Nginx
+sudo apt-get install -y gunicorn
+sudo apt-get install -y python-pip
+
 sudo pip3 install django
+sudo pip install --upgrade django
 sudo pip3 install gunicorn
 sudo -y apt-get install python3-dev libmysqlclient-dev
 sudo -y apt-get install mercurial
--- init.sh
+++ init.sh
@@ -1,9 +1,3 @@
-sudo apt-get update
-sudo apt-get install -y Nginx
-sudo apt-get install -y gunicorn
-sudo apt-get install -y python-pip
-sudo pip install --upgrade django
-
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/wsgi.py /etc/gunicorn.d/wsgi.py
