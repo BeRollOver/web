b260111262a3bae6e9b2be33f9e0a0860df7f0df
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-23 20:15:24 +0300

    gunicorn fix

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/wsgi.py /etc/gunicorn.d/wsgi.py
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn restart
 sudo gunicorn -b 0.0.0.0:80 --pythonpath /home/box/web/ask/ask/ wsgi &
