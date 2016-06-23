849626b94b183b9fd9e3b1a51e036268cf2a6b80
Митя Сизиков <mr.mustard@yandex.ru>
2016-06-23 20:37:21 +0300

    urlconf fix

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/wsgi.py /etc/gunicorn.d/wsgi.py
-sudo gunicorn restart
-sudo gunicorn -b 0.0.0.0:80 --pythonpath /home/box/web/ask/ask/ wsgi &
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask/ask/ wsgi &
